f9ce32260ca234f91bf3304ef87459555fc12d43
Denis <denis-shkurko@yandex.ru>
2022-10-05 19:36:30 +0700

    fhj

--- seminar_5.py
+++ seminar_5.py
@@ -3,23 +3,4 @@ from traceback import print_list
 from unicodedata import name
 os.system("cls")
 
-# № 2. Задайте натуральное число N. Напишите программу, 
-# которая составит список простых множителей числа N.
 
-import math
-num=int(input('Введите число '))
-
-def prime_factors (num):
-  while num%2==0:            #для чётного числа
-    print(2, end=' ')
-    num=num/2
-
-  for i in range(2,int(math.sqrt(num))+1):  #для нечетного числа 
-    while num%i==0:
-      print(i, end=' ')
-      num=num/i
-  if num>2:
-    print(num, end=' ')
-prime_factors(num)
-
-#######################################################
