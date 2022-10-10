b2f99730090b568b522292c23cc4358eeb1c2705
Denis <denis-shkurko@yandex.ru>
2022-10-10 14:06:33 +0700

    1 задача

--- seminar_5.py
+++ seminar_5.py
@@ -3,4 +3,13 @@ from traceback import print_list
 from unicodedata import name
 os.system("cls")
 
+# 1#. Напишите программу, удаляющую из текста
+# все слова, содержащие "абв".
+
+
+text_my = 'самозабвенен мой друг, журнар абвгдейка'
+
+text_my = list(filter(lambda x: 'абв' not in x, text_my.split()))
+my = " ".join(text_my)
+print(my)
 
